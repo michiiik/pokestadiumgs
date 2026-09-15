3fdb98efe5a0ed9199464eafc9f06547368eae22
Michael Keller <49305086+michiiik@users.noreply.github.com>
2026-09-15 18:27:07 +0900

    Fix func_8005C614 call signature

--- src/585D0.c
+++ src/585D0.c
@@ -989,7 +989,7 @@ void func_8005C5CC(s32 arg0, s32 arg1) {
 #endif
 
 #ifdef VERSION_US
-extern s32 func_8005C41C(s32);
+extern void func_8005C41C(s32, s32);
 void func_8005C614(s32 arg0, s32 arg1) {
     switch (func_8005D92C(arg1)) {
         case 1:
@@ -1006,7 +1006,7 @@ void func_8005C614(s32 arg0, s32 arg1) {
             func_8005C5CC(arg0, arg1);
             break;
     }
-    func_8005C41C(arg0);
+    func_8005C41C(arg0, arg1);
 }
 #endif
 
