2eee7330d6b76e00af690a5e0a8855275e837497
Michael Keller <m.keller09101997@gmail.com>
2026-09-15 21:34:53 +0900

    Land container match for func_88200598 (#98)
    Co-authored-by: pokestadium-public-ci[bot] <328905310+pokestadium-public-ci[bot]@users.noreply.github.com>

--- src/fragments/31/fragment31_1736F0.c
+++ src/fragments/31/fragment31_1736F0.c
@@ -4,7 +4,32 @@
 #ifdef VERSION_US
 #pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/31/fragment31_1736F0/func_88200470.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/31/fragment31_1736F0/func_88200598.s")
+extern s32 func_8005370C(s32, s32);
+extern s32 func_8005CF20(s32, s32, s32);
+
+s32 func_88200598(s32 arg0, s32 arg1, s32 arg2) {
+    s32 result = 0;
+
+    switch (arg0) {
+        case 32:
+        case 33:
+        case 34:
+        case 35:
+        case 36:
+        case 37:
+            result = func_8005CF20(arg0, arg1, arg2);
+            break;
+        case 16:
+        case 17:
+        case 18:
+        case 22:
+        case 23:
+            result = func_8005370C(arg0, arg2);
+            break;
+    }
+
+    return result;
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/31/fragment31_1736F0/func_882005F8.s")
 
