73db208365f831e85dccfa18a90b3ca60fb5729b
michiiik <49305086+michiiik@users.noreply.github.com>
2026-09-17 19:30:41 +0900

    Land container match for func_82C00444

--- src/fragments/83/fragment83_code.c
+++ src/fragments/83/fragment83_code.c
@@ -73,7 +73,18 @@ void func_82C003F8(s16 arg0, s16 arg1, u16 arg2, u16 arg3) {
 #endif
 
 #ifdef VERSION_US
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/83/fragment83_code/func_82C00444.s")
+extern void func_80044270(s16, s16, s16, s16, s32, s32, s32, s32, s32);
+extern void func_82C003BC(u16 arg0, u16 arg1);
+void func_82C00444(s16 arg0, s16 arg1, u16 arg2, u16 arg3) {
+    if ((arg2 < 0x20) || (arg3 < 0x20)) return;
+    func_82C003BC(0x101, 4);
+    func_80044270(arg0, arg1, arg2 - 16, 16, 0, 0, 1024, 1024, 0);
+    func_80044270(arg0, arg1 + 16, arg2 - 16, arg3 - 32, 0, 480, 1024, 0, 0);
+    func_80044270(arg0 + arg2 - 16, arg1, 16, 16, 0, 512, 1024, 1024, 0);
+    func_80044270(arg0 + arg2 - 16, arg1 + 16, 16, arg3 - 32, 0, 992, 1024, 0, 0);
+    func_80044270(arg0, arg1 + arg3 - 16, arg2 - 16, 16, 0, 1024, 1024, 1024, 0);
+    func_80044270(arg0 + arg2 - 16, arg1 + arg3 - 16, 16, 16, 0, 1536, 1024, 1024, 0);
+}
 #endif
 
 #ifdef VERSION_US
@@ -184,7 +195,7 @@ void func_82C01FE4(void) {
 #endif
 
 #ifdef VERSION_US
-extern void func_80008648();
+extern void func_80008648(void);
 extern void func_800088DC();
 extern void func_82C00C78();
 extern void func_82C00F20();
