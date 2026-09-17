a89764acc5d3b29daac235fb2ed4a7224eb377b9
Michael Keller <m.keller09101997@gmail.com>
2026-09-17 18:15:25 +0900

    Container match: func_82D03830, func_82D03A5C (#175)
    * Land container match for func_82D03830
    
    * Land container match for func_82D03A5C

--- src/fragments/82/fragment82_4238F0.c
+++ src/fragments/82/fragment82_4238F0.c
@@ -65,7 +65,15 @@ void func_82D03324(s16 arg0, s16 arg1, s32 arg2) {
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/82/fragment82_4238F0/func_82D03778.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/82/fragment82_4238F0/func_82D03830.s")
+extern void func_82D02D20(s32 arg0, s32 arg1);
+extern Gfx* D_800D0510;
+void func_82D03830(s16 arg0, s16 arg1, u16 arg2, u16 arg3) {
+    s32 w = arg2;
+    if (w > 0 && arg3 > 0) {
+        func_82D02D20(0x102, 0);
+        gSPTextureRectangleFlip(D_800D0510++, arg0 * 4, arg1 * 4, (arg0 + w - 1) << 2, (arg1 + arg3 - 1) << 2, G_TX_RENDERTILE, 0, 0, (0x10000 / arg3) & 0xFFFF, 0);
+    }
+}
 
 extern void func_82D03598(s16, s16, u16, u16);
 extern void func_82D03778(s16, s16, s32, s32);
@@ -74,13 +82,29 @@ void func_82D0396C(s16 arg0, s16 arg1, u16 arg2, u16 arg3) {
     func_82D03598(arg0, arg1, arg2, arg3);
 }
 
-extern void func_82D03830(s16, s16, s32, s32);
+extern void func_82D03830(s16, s16, u16, u16);
 void func_82D039E4(s16 arg0, s16 arg1, u16 arg2, u16 arg3) {
     func_82D03830((s16) (arg0 + 3), (s16) (arg1 + 3), (arg2 - 5) & 0xFFFF, (arg3 - 6) & 0xFFFF);
     func_82D03598(arg0, arg1, arg2, arg3);
 }
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/82/fragment82_4238F0/func_82D03A5C.s")
+extern u8 D_82D07248[];
+extern void ParticleGfx_SetPrimColor_fragment82(u8 r, u8 g, u8 b, u8 a);
+extern void func_82D02D5C(s16 arg0, s16 arg1, u16 arg2, u16 arg3);
+extern void func_82D039E4(s16 arg0, s16 arg1, u16 arg2, u16 arg3);
+extern Gfx* D_800D0510;
+void func_82D03A5C(s16 arg0, s16 arg1, u16 arg2, f32 arg3) {
+    s32 x;
+    if (arg2 < 0x14) {
+        return;
+    }
+    func_82D039E4(arg0, arg1, 0x13, arg2);
+    gSPDisplayList(D_800D0510++, D_82D07248);
+    ParticleGfx_SetPrimColor_fragment82(0xFF, 0xFF, 0, 0xFF);
+    x = (s16)(arg0 + 3);
+    func_82D02D5C(x, arg1 + (u16)((f32)(u16)(arg2 - 0x14) * arg3), 0x101, 1);
+    func_82D02D5C(x, (s16)(arg1 + (u16)((f32)(u16)(arg2 - 0x14) * arg3) + 0xA), 0x101, 2);
+}
 
 extern f32 D_80087E50[];
 extern u16 D_82D07280;
