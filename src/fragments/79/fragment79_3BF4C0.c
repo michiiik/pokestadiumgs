6c4f4c62288960729d876ac952d1120806fe108e
michiiik <49305086+michiiik@users.noreply.github.com>
2026-09-26 05:44:13 +0900

    Land container match for func_841528C4

--- src/fragments/79/fragment79_3BF4C0.c
+++ src/fragments/79/fragment79_3BF4C0.c
@@ -233,7 +233,27 @@ void func_841523BC(void *arg0, u8 arg1) {
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/79/fragment79_3BF4C0/func_84152764.s")
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/79/fragment79_3BF4C0/func_841528C4.s")
+extern u8 func_8414A2AC(void);
+extern void * func_8414A3A0(u8);
+extern void * func_84154B64(s32);
+void func_841528C4(void *arg0, u8 arg1) {
+    s32 base;
+    u8 *state;
+    void *table;
+
+    state = func_84154B64((1 - (u32)arg1) & 0xFF);
+    if (!(state[0x18] & 0x10)) {
+        if (*(u16 *)(state + 0x26) & 7) {
+            base = (s32)(u32)arg0 + 4;
+            if (!(state[0x15] & 1)) {
+                *(u8 *)((u8 *)(u32)base + (u32)arg1 + 8) = 0xFF;
+                table = func_8414A3A0(arg1);
+                *(u32 *)((u8 *)(u32)base + ((u32)arg1 << 2) + 0x34) = *(u8 *)((u8 *)table + 0x16) * func_8414A2AC();
+                *(u8 *)(u32)base |= 2;
+            }
+        }
+    }
+}
 
 #pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/79/fragment79_3BF4C0/func_84152980.s")
 
